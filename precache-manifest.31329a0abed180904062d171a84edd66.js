5d39738116deb08c97e36651f799f76fe5dd9765
Mabel Chan <mabelcha@umich.edu>
2020-05-12 00:27:00 -0400

    Updates

---
+++ precache-manifest.31329a0abed180904062d171a84edd66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8ef5833c8289b9561764d117d39397d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5f916fffc40820fcdc5",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "10c83b474b17a975cc64",
+    "url": "/static/js/2.23540f3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.23540f3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5f916fffc40820fcdc5",
+    "url": "/static/js/main.6505c606.chunk.js"
+  },
+  {
+    "revision": "fcba7d7f92bf98b8260d",
+    "url": "/static/js/runtime-main.d3de3a57.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
